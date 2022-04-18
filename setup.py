2eb6177673fc6e772cab192ea0f01dfad557c770
PoeticViper <PoeticViper@users.noreply.github.com>
2022-04-18 07:48:32 -0500

    added to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from pathlib import Path
 
 
 __version__ = 1.0
@@ -6,10 +7,16 @@ __author__ = 'PoeticViper'
 __credits__ = 'PoeticViper'
 
 
+cur_dir = Path(__file__).parent
+long_description = (cur_dir / 'README.md').read_text()
+
+
 setup(
     name='vndb_api_wrapper',
-    version='1.0.0',
+    version='1.0.0a',
     description='A wrapper for the VNDB API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/PoeticViper/VNDB-Python-API-Wrapper',
     author='PoeticViper',
     author_email='PoeticViper@users.noreply.github.com',
